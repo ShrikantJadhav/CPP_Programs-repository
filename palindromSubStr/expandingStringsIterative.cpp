a37ead749f37620f0578293d1963c2d343637a6e
shrikant <shrikant.jadhav1@gmail.com>
2018-06-24 16:27:06 +0530

    Playing around

---
+++ palindromSubStr/expandingStringsIterative.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<string>
+#include<iterator>
+#include<algorithm>
+
+using namespace std;
+
+void expandToCheckPalindrome(string str, int i, int j, 
+			     set<string>& setPalindrome)
+{
+    if( i < 0 || j >= str.size() )
+    	return;
+
+    while(i >= 0 && j < str.size() &&
+    	    str[i] == str[j])
+    {
+    	setPalindrome.insert(str.substr(i,j-i+1));
+    	--i;
+    	++j;
+    }
+    return;
+}
+
+void printAllPalindromicSubstr(string str)
+{
+    set<string> setPalindrome;
+    for(int i = 0; i < str.size(); ++i)
+    {
+    	expandToCheckPalindrome(str,i,i,setPalindrome);
+    	expandToCheckPalindrome(str,i,i+1,setPalindrome);
+    }
+
+    copy(setPalindrome.begin(), setPalindrome.end(),
+    	    ostream_iterator<string>(cout,","));	
+    cout<<endl;
+}
+
+int main()
+{
+    string str = "abaaba";
+    cout<<"String = "<<str<<" - List of all palindromic substr = ";
+    printAllPalindromicSubstr(str);
+
+    string str1 = "ababbccbb";
+    cout<<"String = "<<str1<<" - List of all palindromic substr = ";
+    printAllPalindromicSubstr(str1);
+}
